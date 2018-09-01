27141f6160c2891d762bf95bff350a047bb3ef90
to-yamada <5902143+to-yamada@users.noreply.github.com>
2018-09-01 15:44:08 +0900

    上下移動はリンクにする必要性がなさそうなため、<a>要素を削除

--- scroll.js
+++ scroll.js
@@ -1,13 +1,11 @@
 $(function() {
   // 最上部へ移動
   $('#page-top').click(function(e) {
-    e.preventDefault();
     $('html, body').animate({scrollTop:0}, 'slow');
   });
 
   // 最下部へ移動
   $('#page-bottom').click(function(e) {
-    e.preventDefault();
     $('html, body').animate({scrollTop:$(document).height()}, 'slow');
   });
 });
